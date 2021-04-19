8a38518f7d65146c6d847937e097add9ae12e54e
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-04-20 02:33:07 +0600

    Dynamic class componenet and Dynamic function component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './components/Person.js';
 
-function App() {
-    // return (
-    //     <div className="App" >
-    //         <h1>
-    //             My First React App
-    //         </h1>
-    //         <Person />
-    //     </div>
-    // );
-    return React.createElement('div', { className: "App" }, React.createElement('h1', null, "My First React App"), <Person />)
-}
+class App extends Component {
+    render() {
+        
+        return (
+            <div className="App" >
+                <h1>
+                    My First React App
+                </h1>
+                <Person name="Touhidul Islam" age="31" profession="Full-Stack Web Developer" />
+                <Person name="Ariful Alama" age="35" profession="Back-End Developer" />
+                <Person name="AHM Tareq" age="26" profession="Full-Stack Web Developer" />
+            </div>
 
-function Person() {
-    return (
-        <div>
-            <h1>
-                I am person component
-            </h1>
-        </div>
-    );
+        );
+    }
+    //return React.createElement('div', { className: "App" }, React.createElement('h1', null, "My First React App"), <Person />)
 }
 
 export default App;
