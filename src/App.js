2cd34ceb93a375330af6256a48b2c6784d31e19e
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-04-21 16:53:28 +0600

    Change State with js event

--- src/App.js
+++ src/App.js
@@ -12,30 +12,34 @@ class App extends Component {
             { bookName: "Bukhari Sharif", price: "500" },
             { bookName: "Muslim Sharif", price: "500" },
             { bookName: "Kholafaye Rashedin", price: "1200" }
-        ],
-        myState: "My State"
-    };
-    // constructor() {
-    //     super();
-    //     this.state = {};
-    // }
+        ]
+    }
+
+    changeBookState = () => {
+        this.setState({
+            books: [
+                { bookName: "AFFFFA", price: "100" },
+                { bookName: "hDASSh", price: "500" },
+                { bookName: "test", price: "500" },
+                { bookName: "Kholafaye Rashedin", price: "1200" }
+            ]
+        });
+    }
     render() {
+        console.log(this.state);
         return (
             <div className="App" >
                 <h1>
                     Book List
                 </h1>
-                {/* <Person name="Touhidul Islam" age="31" profession="Full-Stack Web Developer" />
-                <Person name="Ariful Alama" age="35" profession="Back-End Developer" />
-                <Person name="AHM Tareq" age="26" profession="Full-Stack Web Developer" /> */}
-                <Book nameName={this.state.books[0].bookName} price={this.state.books[0].price} />
-                <Book nameName={this.state.books[1].bookName} price={this.state.books[1].price} />
-                <Book nameName={this.state.books[2].bookName} price={this.state.books[2].price} />
-                <Book nameName={this.state.books[3].bookName} price={this.state.books[3].price} />
+                <button onClick={this.changeBookState}>Change state</button>
+                <Book bookName={this.state.books[0].bookName} price={this.state.books[0].price} />
+                <Book bookName={this.state.books[1].bookName} price={this.state.books[1].price} />
+                <Book bookName={this.state.books[2].bookName} price={this.state.books[2].price} />
+                <Book bookName={this.state.books[3].bookName} price={this.state.books[3].price} />
             </div>
         );
     }
-    //return React.createElement('div', { className: "App" }, React.createElement('h1', null, "My First React App"), <Person />)
 }
 
 export default App;
