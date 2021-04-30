a1e618fd8bfac9bcaa6c23e354a2e2ecb3ac4b1c
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-04-30 17:15:38 +0600

    Uncontrolled form in react

--- src/components/representational/NewBook.js
+++ src/components/representational/NewBook.js
@@ -5,28 +5,34 @@ import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 class NewBook extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            bookName: "",
-            writer: "",
-            price: 0
-        }
-        this.handleInputChange = this.handleInputChange.bind(this);
+        // this.state = {
+        //     bookName: "",
+        //     writer: "",
+        //     price: 0
+        // }
+        // this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.bookName = React.createRef();
+        this.writer = React.createRef();
+        this.price = React.createRef();
+
     }
 
     handleSubmit = event => {
-        console.log(this.state)
+        console.log(this.bookName.current.value);
+        console.log(this.writer.current.value);
+        console.log(this.price.current.value);
         event.preventDefault();
     }
 
 
-    handleInputChange = event => {
-        const name = event.target.name;
-        const value = event.target.value;
-        this.setState({
-            [name]: value
-        });
-    }
+    // handleInputChange = event => {
+    //     const name = event.target.name;
+    //     const value = event.target.value;
+    //     this.setState({
+    //         [name]: value
+    //     });
+    // }
 
     render() {
         return (
@@ -34,15 +40,15 @@ class NewBook extends Component {
                 <Form onSubmit={this.handleSubmit}>
                     <FormGroup>
                         <Label for="bookName" sm={2}>Book Name:</Label>
-                        <Input type="text" name="bookName" id="bookName" placeholder="enter book name" value={this.state.bookName} onChange={this.handleInputChange} />
+                        <Input type="text" name="bookName" placeholder="enter book name" innerRef={this.bookName} />
                     </FormGroup>
                     <FormGroup>
                         <Label for="writer" sm={2}>Writer</Label>
-                        <Input type="text" name="writer" id="writer" placeholder="enter writer name" value={this.state.writer} onChange={this.handleInputChange} />
+                        <Input type="text" name="writer" placeholder="enter writer name" innerRef={this.writer} />
                     </FormGroup>
                     <FormGroup>
                         <Label for="price" sm={2}>Price</Label>
-                        <Input type="number" name="price" id="writer" placeholder="input book price" value={this.state.price} onChange={this.handleInputChange} />
+                        <Input type="number" name="price" placeholder="input book price" innerRef={this.price} />
                     </FormGroup>
                     <Button>Submit</Button>
                 </Form>
