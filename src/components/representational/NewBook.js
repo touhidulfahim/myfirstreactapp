359ea4eddc2f6c1106d1f55fd97db9693e25da61
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-04-30 16:40:16 +0600

    Taking care submited data

--- src/components/representational/NewBook.js
+++ src/components/representational/NewBook.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 
 class NewBook extends Component {
@@ -10,36 +10,39 @@ class NewBook extends Component {
             writer: "",
             price: 0
         }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleSubmit = event => {
+        console.log(this.state)
+        event.preventDefault();
+    }
+
+
     handleInputChange = event => {
         const name = event.target.name;
         const value = event.target.value;
-
         this.setState({
             [name]: value
         });
     }
 
-    componentDidUpdate() {
-        console.log(this.state)
-    }
-
-
     render() {
         return (
             <div className="container" >
-                <Form>
+                <Form onSubmit={this.handleSubmit}>
                     <FormGroup>
                         <Label for="bookName" sm={2}>Book Name:</Label>
-                        <Input type="text" name="bookName" id="bookName" placeholder="enter book name" value={this.state.bookName} onChange={event => this.handleInputChange(event)} />
+                        <Input type="text" name="bookName" id="bookName" placeholder="enter book name" value={this.state.bookName} onChange={this.handleInputChange} />
                     </FormGroup>
                     <FormGroup>
                         <Label for="writer" sm={2}>Writer</Label>
-                        <Input type="text" name="writer" id="writer" placeholder="enter writer name" value={this.state.writer} onChange={event => this.handleInputChange(event)} />
+                        <Input type="text" name="writer" id="writer" placeholder="enter writer name" value={this.state.writer} onChange={this.handleInputChange} />
                     </FormGroup>
                     <FormGroup>
                         <Label for="price" sm={2}>Price</Label>
-                        <Input type="number" name="price" id="writer" placeholder="input book price" value={this.state.price} onChange={event => this.handleInputChange(event)} />
+                        <Input type="number" name="price" id="writer" placeholder="input book price" value={this.state.price} onChange={this.handleInputChange} />
                     </FormGroup>
                     <Button>Submit</Button>
                 </Form>
